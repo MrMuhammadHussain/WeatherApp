f13702f48381ff000c79bfbe04443ec563a32806
MuhammadHussain <34907815+MrMuhammadHussain@users.noreply.github.com>
2022-09-23 15:07:14 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let apikey = "adb4676581b7a9b11a6c8ae382b6dcf3";
 function getweather() {
     let cityname = document.querySelector("#cityname").value;
-    axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=${apikey}units=matrix`&units=metric)
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=${apikey}&units=metric`)
         .then(function (response) {
             document.querySelector("#data").innerHTML = `${response.data.name} Temp is ${response.data.main.temp}°C`;
             console.log(response.data);
